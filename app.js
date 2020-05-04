0557e46da708838844eb609dcbe9e73fad6e93f6
theogilardo <gilardotheo@gmail.com>
2020-05-04 15:32:12 +0200

    modif content

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
     //3. Check if Winner
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 50) {
       document.querySelector('#name-' + activePlayer).textContent = 'WINNER';
       diceNone()
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
